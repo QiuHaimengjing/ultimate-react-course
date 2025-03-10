68d5cf87ebe288c6e53138da12ac210d23a32f60
qiu <qqdas522@gmail.com>
2025-03-10 10:54:51 +0800

    Building the Pages

--- 10-worldwise/src/App.jsx
+++ 10-worldwise/src/App.jsx
@@ -3,14 +3,18 @@ import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
