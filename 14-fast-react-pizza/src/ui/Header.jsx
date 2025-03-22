536f29aba02c8d0de85f7b805c8d31e299a6b424
qiu <qqdas522@gmail.com>
2025-03-22 17:32:42 +0800

    Handling Errors With Error Elements. Fetching Orders.

--- 14-fast-react-pizza/src/ui/Header.jsx
+++ 14-fast-react-pizza/src/ui/Header.jsx
@@ -1,7 +1,13 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
-  return <Link to="/">Fast React Pizza Co.</Link>;
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
+    </header>
+  );
 }
 
 export default Header;
