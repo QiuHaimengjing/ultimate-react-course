1de783399dd33a735206ab856e95baadf9abd3f2
qiu <qqdas522@gmail.com>
2025-04-02 11:35:47 +0800

    Styled Component Props and the css Function.

--- 15-the-wild-oasis/src/App.jsx
+++ 15-the-wild-oasis/src/App.jsx
@@ -2,15 +2,10 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.main`
-  background-color: orangered;
   padding: 20px;
 `;
 
@@ -19,9 +14,24 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>Welcome</H1>
-        <Button>Click</Button>
-        <Input placeholder="Input" />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <Heading as="h2">Check in and out</Heading>
+            <Button>Check in</Button>
+            <Button variation="secondary" size="small">
+              Check out
+            </Button>
+          </Row>
+
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form action="">
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
