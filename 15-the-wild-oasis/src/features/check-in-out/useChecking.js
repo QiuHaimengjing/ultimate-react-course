78a920bbb879aeaba400bee573600dd43b5b1ccd
qiu <qqdas522@gmail.com>
2025-05-09 14:36:56 +0800

    Checking In a Booking.

---
+++ 15-the-wild-oasis/src/features/check-in-out/useChecking.js
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import { updateBooking } from "../../services/apiBookings";
+
+export function useChecking() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const { mutate: checkin, isLoading: isCheckingIn } = useMutation({
+    mutationFn: (bookingId) =>
+      updateBooking(bookingId, { status: "checked-in", isPaid: true }),
+
+    onSuccess: (data) => {
+      toast.success(`Booking #${data.id} has been checked in successfully!`);
+      queryClient.invalidateQueries({ active: true });
+      navigate("/");
+    },
+
+    onError: () => toast.error("There was an error while checking in"),
+  });
+
+  return { checkin, isCheckingIn };
+}
